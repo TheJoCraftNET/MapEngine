0f9e6a1447202ed3e3a16f3647b0bf402e925fe0
pianoman911 <pianofinn13@gmail.com>
2023-04-10 21:41:56 +0200

    Added optional antialiasing for text conversion

--- api/src/main/java/de/pianoman911/mapengine/api/util/FontRegistry.java
+++ api/src/main/java/de/pianoman911/mapengine/api/util/FontRegistry.java
@@ -2,8 +2,8 @@ package de.pianoman911.mapengine.api.util;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class FontRegistry {
@@ -11,14 +11,22 @@ public class FontRegistry {
     public static final Font DEFAULT = new Font("Arial", Font.PLAIN, 10);
 
     public static BufferedImage convertText(String text, Font font, Color color) {
+        return convertText(text, font, color, true);
+    }
+
+    public static BufferedImage convertText(String text, Font font, Color color, boolean antiAliasing) {
         if (font == null) {
             font = DEFAULT;
         }
 
         BufferedImage b = new BufferedImage(text.length() * font.getSize(), (int) (font.getSize() * (5 / 4.0)), BufferedImage.TYPE_INT_ARGB);
-        Graphics g = b.getGraphics();
+        Graphics2D g = (Graphics2D) b.getGraphics();
         g.setFont(font);
         g.setColor(color);
+        if (antiAliasing) {
+            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        }
+
         g.drawString(text, 0, font.getSize());
         g.dispose();
 
@@ -28,7 +36,7 @@ public class FontRegistry {
         int endY = 0;
         for (int x = 0; x < b.getWidth(); x++) {
             for (int y = 0; y < b.getHeight(); y++) {
-                if (((b.getRGB(x, y) >> 24) & 0xFF) == 255) {
+                if (((b.getRGB(x, y) >> 24) & 0xFF) != 0) {
                     startX = Math.min(startX, x);
                     startY = Math.min(startY, y);
                     endX = Math.max(endX, x);
@@ -43,11 +51,16 @@ public class FontRegistry {
         Graphics2D gc = bc.createGraphics();
         gc.drawImage(b, 0, 0, endX - startX, endY - startY, startX, startY, endX, endY, null);
         gc.dispose();
+
         return bc;
     }
 
     public static FullSpacedColorBuffer convertText2Bytes(String text, Font font, Color color) {
-        BufferedImage image = convertText(text, font, color);
+        return convertText2Bytes(text, font, color, true);
+    }
+
+    public static FullSpacedColorBuffer convertText2Bytes(String text, Font font, Color color, boolean antiAliasing) {
+        BufferedImage image = convertText(text, font, color, antiAliasing);
         int[] bytes = new int[image.getWidth() * image.getHeight()];
         image.getRGB(0, 0, image.getWidth(), image.getHeight(), bytes, 0, image.getWidth());
         return new FullSpacedColorBuffer(bytes, image.getWidth(), image.getHeight());
