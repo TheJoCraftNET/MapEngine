7a3dfcf0d77588d852911176efcf7b0f7e56dd4f
booky10 <boooky10@gmail.com>
2024-04-27 17:38:14 +0200

    Remove shadow support workaround in 1.20.5 platform
    Still required for older versions to shadow the reobfuscated jars

--- platform-paper-1.20.5/build.gradle.kts
+++ platform-paper-1.20.5/build.gradle.kts
@@ -13,20 +13,6 @@ java {
 }
 
 tasks {
-    reobfJar {
-        inputJar.set(jar.get().outputs.files.singleFile)
-        outputJar.set(File.createTempFile("reobfOut", ".jar"))
-
-        doLast {
-            val inputFile = inputJar.get().asFile
-            inputFile.delete()
-
-            val outputFile = outputJar.get().asFile
-            outputFile.copyTo(inputFile)
-            outputFile.delete()
-        }
-    }
-
     assemble {
         dependsOn(reobfJar)
     }
