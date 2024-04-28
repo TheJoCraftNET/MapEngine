0944020db3274bc300dcd05ab66e9269586bb111
booky10 <boooky10@gmail.com>
2024-04-28 04:27:24 +0200

    Fix 1.20.5 platform detection
    Paper no longer relocates CraftBukkit classes with 1.20.5

--- platform-paper-1.20.5/src/main/java/de/pianoman911/mapengine/common/Paper1205Provider.java
+++ platform-paper-1.20.5/src/main/java/de/pianoman911/mapengine/common/Paper1205Provider.java
@@ -3,15 +3,18 @@ package de.pianoman911.mapengine.common;
 import de.pianoman911.mapengine.common.platform.IListenerBridge;
 import de.pianoman911.mapengine.common.platform.IPlatform;
 import de.pianoman911.mapengine.common.platform.IPlatformProvider;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Optional;
 
 public final class Paper1205Provider implements IPlatformProvider {
 
+    @SuppressWarnings("deprecation") // bukkit unsafe
     @Override
     public Optional<IPlatform<?>> tryProvide(Plugin plugin, IListenerBridge bridge) {
-        if (IPlatformProvider.existsClass("org.bukkit.craftbukkit.v1_20_R3.CraftServer")) {
+        if (IPlatformProvider.existsClass("org.bukkit.craftbukkit.CraftServer")
+                && Bukkit.getUnsafe().getProtocolVersion() == 766) {
             return Optional.of(Paper1205StaticProvider.provide(plugin, bridge));
         }
         return Optional.empty();
